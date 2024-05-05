0d15b0a809b9c5da45252795aa8f7422b020c14b
Aakash Jain <aakashvjain2003@gmail.com>
2024-05-05 22:14:07 +0530

    added

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import tensorflow as tf
+
+from transformers import  PegasusTokenizer
+import torch
+
+app = Flask(__name__)
+model_dir = r"..\Text-summarization\pegasus-samsum-model"
+tokenizer = PegasusTokenizer.from_pretrained(r"..\Text-summarization\tokenizer")
+print("Tokenizer loaded")
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+from transformers import pipeline
+gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+
+pipe = pipeline("summarization", model=model_dir,tokenizer=tokenizer)
+print("Done")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/text-summarization', methods=["POST"])
+def summarize():
+    if request.method == "POST":
+        input_text = request.form["inputtext_"]
+
+        # Use the summarization pipeline to generate the summary
+        summary = pipe(input_text, **gen_kwargs)[0]['summary_text']
+
+    return render_template("output.html", data={"summary": summary})
+
+if __name__ == '__main__': # It Allows You to Execute Code When the File Runs as a Script
+    app.run()
